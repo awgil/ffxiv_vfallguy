e0a7b6d852efd69e2e6be02c5aac9310df093f32
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2023-11-05 08:49:27 +0000

    Window refactoring - make sure overlays are always drawn.

--- vfallguy/MainWindow.cs
+++ vfallguy/MainWindow.cs
@@ -15,7 +15,10 @@ public class MainWindow : Window, IDisposable
     private DebugDrawer _drawer = new();
     private AutoJoinLeave _automation = new();
     private Map? _map;
+    private DateTime _now;
     private Vector3 _prevPos;
+    private Vector3 _movementDirection;
+    private float _movementSpeed;
     private bool _autoJoin;
     private bool _autoLeaveIfNotSolo;
     private DateTime _autoJoinAt = DateTime.MaxValue;
@@ -37,12 +40,68 @@ public class MainWindow : Window, IDisposable
         _automation.Dispose();
     }
 
-    public override void PreOpenCheck()
+    public unsafe override void PreOpenCheck()
     {
         _automation.Update();
+        _drawer.Update();
+
+        _now = DateTime.Now;
+        var playerPos = Service.ClientState.LocalPlayer?.Position ?? new();
+        _prevPos = playerPos;
+        _movementDirection = playerPos - _prevPos;
+        _movementSpeed = _movementDirection.Length() / Framework.Instance()->FrameDeltaTime;
+        _movementDirection = _movementDirection.NormalizedXZ();
 
         IsOpen = Service.ClientState.TerritoryType is 1165 or 1197 && !Service.Condition[ConditionFlag.BetweenAreas];
 
+        UpdateMap();
+        UpdateAutoJoin();
+        UpdateAutoLeave();
+        DrawOverlays();
+
+        _drawer.DrawWorldPrimitives();
+    }
+
+    public unsafe override void Draw()
+    {
+        if (ImGui.Button("Queue"))
+            _automation.RegisterForDuty();
+        ImGui.SameLine();
+        if (ImGui.Button("Leave"))
+            _automation.LeaveDuty();
+        ImGui.SameLine();
+        ImGui.TextUnformatted($"Num players in duty: {_numPlayersInDuty} (autoleave: {(_autoLeaveAt == DateTime.MaxValue ? "never" : $"in {(_autoLeaveAt - _now).TotalSeconds:f1}s")})");
+
+        ImGui.Checkbox("Auto register", ref  _autoJoin);
+        if (_autoJoin)
+        {
+            ImGui.SameLine();
+            ImGui.SliderFloat("Delay###j", ref _autoJoinDelay, 0, 10);
+        }
+        ImGui.Checkbox("Auto leave if not solo", ref _autoLeaveIfNotSolo);
+        if (_autoLeaveIfNotSolo)
+        {
+            ImGui.SameLine();
+            ImGui.SliderFloat("Delay###l", ref _autoLeaveDelay, 0, 10);
+        }
+
+        if (_map != null)
+        {
+            ImGui.TextUnformatted($"Pos: {_map.PlayerPos}");
+            ImGui.TextUnformatted($"Path: {_map.Path.Count}");
+            ImGui.TextUnformatted($"Speed: {_movementSpeed}");
+
+            foreach (var aoe in _map.AOEs.Where(aoe => aoe.NextActivation != default))
+            {
+                var nextActivation = (aoe.NextActivation - _now).TotalSeconds;
+                using (ImRaii.PushColor(ImGuiCol.Text, nextActivation < 0 ? 0xff0000ff : 0xffffffff))
+                    ImGui.TextUnformatted($"{aoe.Type} R{aoe.R1} @ {aoe.Origin}: activate in {nextActivation:f3}, repeat={aoe.Repeat}, seqd={aoe.SeqDelay}");
+            }
+        }
+    }
+
+    private void UpdateMap()
+    {
         Type? mapType = null;
         if (IsOpen)
         {
@@ -66,23 +125,31 @@ public class MainWindow : Window, IDisposable
                 _map = (Map?)Activator.CreateInstance(mapType, _gameEvents);
         }
 
+        _map?.Update();
+    }
+
+    private void UpdateAutoJoin()
+    {
         bool wantAutoJoin = _autoJoin && _automation.Idle && IsOpen && Service.ClientState.TerritoryType == 1197 && !Service.Condition[ConditionFlag.WaitingForDutyFinder] && !Service.Condition[ConditionFlag.BetweenAreas];
         if (!wantAutoJoin)
         {
             _autoJoinAt = DateTime.MaxValue;
         }
-        else if (_autoJoinAt ==  DateTime.MaxValue)
+        else if (_autoJoinAt == DateTime.MaxValue)
         {
             Service.Log.Info($"Auto-joining in {_autoJoinDelay:f2}s...");
-            _autoJoinAt = DateTime.Now.AddSeconds(_autoJoinDelay);
+            _autoJoinAt = _now.AddSeconds(_autoJoinDelay);
         }
-        else if (DateTime.Now >= _autoJoinAt)
+        else if (_now >= _autoJoinAt)
         {
             Service.Log.Info($"Auto-joining");
             _automation.RegisterForDuty();
             _autoJoinAt = DateTime.MaxValue;
         }
+    }
 
+    private void UpdateAutoLeave()
+    {
         _numPlayersInDuty = Service.ClientState.TerritoryType == 1165 && Service.Condition[ConditionFlag.BoundByDuty] && !Service.Condition[ConditionFlag.BetweenAreas]
             ? Service.ObjectTable.Count(o => o.ObjectKind == Dalamud.Game.ClientState.Objects.Enums.ObjectKind.Player)
             : 0;
@@ -94,9 +161,9 @@ public class MainWindow : Window, IDisposable
         else if (_autoLeaveAt == DateTime.MaxValue)
         {
             Service.Log.Info($"Auto-leaving in {_autoLeaveDelay:f2}s...");
-            _autoLeaveAt = DateTime.Now.AddSeconds(_autoLeaveDelay);
+            _autoLeaveAt = _now.AddSeconds(_autoLeaveDelay);
         }
-        else if (DateTime.Now >= _autoLeaveAt)
+        else if (_now >= _autoLeaveAt)
         {
             Service.Log.Info($"Auto-leaving: {_numPlayersInDuty} players");
             _automation.LeaveDuty();
@@ -104,79 +171,41 @@ public class MainWindow : Window, IDisposable
         }
     }
 
-    public unsafe override void Draw()
+    private void DrawOverlays()
     {
-        if (ImGui.Button("Queue"))
-            _automation.RegisterForDuty();
-        ImGui.SameLine();
-        if (ImGui.Button("Leave"))
-            _automation.LeaveDuty();
-        ImGui.SameLine();
-        ImGui.TextUnformatted($"Num players in duty: {_numPlayersInDuty} (autoleave: {(_autoLeaveAt == DateTime.MaxValue ? "never" : $"in {(_autoLeaveAt - DateTime.Now).TotalSeconds:f1}s")})");
+        if (_map == null)
+            return;
 
-        ImGui.Checkbox("Auto register", ref  _autoJoin);
-        if (_autoJoin)
+        var from = _map.PlayerPos;
+        for (int i = 0; i < _map.Path.Count; ++i)
         {
-            ImGui.SameLine();
-            ImGui.SliderFloat("Delay###j", ref _autoJoinDelay, 0, 10);
-        }
-        ImGui.Checkbox("Auto leave if not solo", ref _autoLeaveIfNotSolo);
-        if (_autoLeaveIfNotSolo)
-        {
-            ImGui.SameLine();
-            ImGui.SliderFloat("Delay###l", ref _autoLeaveDelay, 0, 10);
+            var wp = _map.Path[i];
+            var delay = (wp.StartMoveAt - _now).TotalSeconds;
+            _drawer.DrawWorldLine(from, wp.Dest, i > 0 ? 0xff00ffff : delay <= 0 ? 0xff00ff00 : 0xff0000ff);
+            if (delay > 0)
+                _drawer.DrawWorldText(from, 0xff0000ff, $"{delay:f3}");
+            from = wp.Dest;
         }
 
-        if (_map != null)
+        foreach (var aoe in _map.AOEs.Where(aoe => aoe.NextActivation != default))
         {
-            _map.Update();
-            ImGui.TextUnformatted($"Pos: {_map.PlayerPos}");
-            ImGui.TextUnformatted($"Path: {_map.Path.Count}");
-
-            var movement = _map.PlayerPos - _prevPos;
-            var speed = movement.Length() / Framework.Instance()->FrameDeltaTime;
-            ImGui.TextUnformatted($"Speed: {speed}");
-            _prevPos = _map.PlayerPos;
-
-            _drawer.Update();
-
-            var from = _map.PlayerPos;
-            var now = DateTime.Now;
-            for (int i = 0; i < _map.Path.Count; ++i)
+            var nextActivation = (aoe.NextActivation - _now).TotalSeconds;
+            if (nextActivation < 2.5f)
             {
-                var wp = _map.Path[i];
-                var delay = (wp.StartMoveAt - now).TotalSeconds;
-                _drawer.DrawWorldLine(from, wp.Dest, i > 0 ? 0xff00ffff : delay <= 0 ? 0xff00ff00 : 0xff0000ff);
-                if (delay > 0)
-                    _drawer.DrawWorldText(from, 0xff0000ff, $"{delay:f3}");
-                from = wp.Dest;
-            }
-
-            var moveDir = movement.NormalizedXZ();
-            foreach (var aoe in _map.AOEs.Where(aoe => aoe.NextActivation != default))
-            {
-                var nextActivation = (aoe.NextActivation - now).TotalSeconds;
-                using (ImRaii.PushColor(ImGuiCol.Text, nextActivation < 0 ? 0xff0000ff : 0xffffffff))
-                    ImGui.TextUnformatted($"{aoe.Type} R{aoe.R1} @ {aoe.Origin}: activate in {nextActivation:f3}, repeat={aoe.Repeat}, seqd={aoe.SeqDelay}");
-                if (nextActivation < 2.5f)
+                bool risky = false;
+                if (_movementSpeed > 0)
                 {
-                    bool risky = false;
-                    if (speed > 0)
-                    {
-                        var (aoeEnter, aoeExit) = aoe.Intersect(_map.PlayerPos, moveDir);
-                        if (!float.IsNaN(aoeEnter) && aoe.ActivatesBetween(now, aoeEnter * Map.InvSpeed, aoeExit * Map.InvSpeed) is var delay && delay > 0)
-                            risky = true;
-                    }
-
-                    aoe.Draw(_drawer, risky ? 0xff0000ff : 0xff00ffff);
-                    var dir = (aoe.Origin - _map.PlayerPos).NormalizedXZ();
-                    var (enter, exit) = aoe.Intersect(_map.PlayerPos, dir);
-                    var textPos = _map.PlayerPos + dir * MathF.Max(enter, 0);
-                    _drawer.DrawWorldText(textPos, risky ? 0xff0000ff : 0xff00ffff, $"{nextActivation:f3}");
+                    var (aoeEnter, aoeExit) = aoe.Intersect(_map.PlayerPos, _movementDirection);
+                    if (!float.IsNaN(aoeEnter) && aoe.ActivatesBetween(_now, aoeEnter * Map.InvSpeed, aoeExit * Map.InvSpeed) is var delay && delay > 0)
+                        risky = true;
                 }
-            }
 
-            _drawer.DrawWorldPrimitives();
+                aoe.Draw(_drawer, risky ? 0xff0000ff : 0xff00ffff);
+                var dir = (aoe.Origin - _map.PlayerPos).NormalizedXZ();
+                var (enter, exit) = aoe.Intersect(_map.PlayerPos, dir);
+                var textPos = _map.PlayerPos + dir * MathF.Max(enter, 0);
+                _drawer.DrawWorldText(textPos, risky ? 0xff0000ff : 0xff00ffff, $"{nextActivation:f3}");
+            }
         }
     }
 }
